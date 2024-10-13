5bb63458737bfe7892821fd6cd140814c9027d5b
Adi-Goll <adigollamudi@gmail.com>
2024-10-12 21:54:49 -0700

    basic parsing logic for important segments

--- parser.py
+++ parser.py
@@ -1,14 +1,31 @@
-import hl7
+file_path = "hl7Examples/allergen.hl7"
 
-message = "whatever we scan in"
+with open(file_path, 'r') as file:
+    message = file.read()
 
-returnObj = hl7.parse(message)
+#message = "MSH|^~\\&|SENDING_APPLICATION|SENDING_FACILITY|RECEIVING_APPLICATION|RECEIVING_FACILITY|202212290801||ADT^A01|93457|P|2.5|||||"
+
+lines = message.split('\n')
+
+# patient info, allergen info, insurance, disability, diagnosis, pharamacy treatment
+PID = AL1 = IN1 = DB1 = DG1 = RX = None
 
 
-msh_segment = returnObj[0]
-evn_segment = returnObj[1]
-pid_segment = returnObj[2]
+for i in range(len(lines)):
+    lines[i]= lines[i].split('|')    
+    if lines[i][0] == "PID":
+        PID = ["NAME", lines[i][5].split("^"), "SEX", lines[i][8]];
+    if lines[i][0] == "AL1":
+        AL1 = lines[i][2].split("^")
+        print(AL1)
+    if lines[i][0] == "IN1":
+        IN1 = lines[i][2].split("^")
+        print(IN1)
+    if lines[i][0] == "DB1":
+        DB1 = lines[i]
+    if lines[i][0] == "DG1":
+        DG1 = lines[i]
+    if 'RX' in lines[i][0]:
+        RX += lines[i]
 
-# Access the fields of a segment
-sending_application = msh_segment[2]
-patient_id = pid_segment[2]
+# print(splitMsg)
